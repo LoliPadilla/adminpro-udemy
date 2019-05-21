4777bd2ed7c1e71c754fb1e07c01beccce3fb802
Loli Padilla <lolilasaga@hotmail.com>
2019-05-21 14:22:43 -0300

    modificaciones en los modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,40 +1,23 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { APP_ROUTES } from "./app.routes";
 
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { APP_ROUTES } from './app.routes';
+import { PagesModule } from "./pages/pages.module";
 
-import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { PagesComponent } from './pages/pages.component';
-import { RegisterComponent } from './login/register.component';
-
+import { AppComponent } from "./app.component";
+import { LoginComponent } from "./login/login.component";
+import { NopagefoundComponent } from "./pages/nopagefound/nopagefound.component";
+import { RegisterComponent } from "./login/register.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     NopagefoundComponent,
-    Graficas1Component,
-    HeaderComponent,
-    SidebarComponent,
-    BreadcrumbsComponent,
-    DashboardComponent,
-    ProgressComponent,
-    PagesComponent,
     RegisterComponent
   ],
-  imports: [
-    BrowserModule,
-    APP_ROUTES
-  ],
+  imports: [PagesModule, BrowserModule, APP_ROUTES],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
